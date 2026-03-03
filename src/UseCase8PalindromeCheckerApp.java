611ab550169b2c4ccdfd9f37375b766b66c7ce20
apoorwa46 <apoorwa466@gmail.com>
2026-03-03 15:11:03 +0530

    UC8

---
+++ src/UseCase8PalindromeCheckerApp.java
@@ -0,0 +1,66 @@
+public class UseCase8PalindromeCheckerApp {
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String text = "banana";
+        Node head = null, tail = null;
+
+        for (int i = 0; i < text.length(); i++) {
+            Node newNode = new Node(text.charAt(i));
+
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        Node current = slow;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        Node firstHalf = head;
+        Node secondHalf = prev;
+
+        boolean isPalindrome = true;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                isPalindrome = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The given string \"" + text + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + text + "\" is NOT a Palindrome.");
+        }
+    }
+}
