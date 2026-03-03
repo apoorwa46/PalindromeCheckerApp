e1bae079cbcc3f3fe3acbe9b283f216cd33221f5
apoorwa46 <apoorwa466@gmail.com>
2026-03-03 15:11:45 +0530

    UC9

---
+++ src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+public class UseCase9PalindromeCheckerApp {
+
+    static boolean isPalindrome(String text, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (text.charAt(start) != text.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(text, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String text = "solos";
+
+        boolean result = isPalindrome(text, 0, text.length() - 1);
+
+        if (result) {
+            System.out.println("The given string \"" + text + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + text + "\" is NOT a Palindrome.");
+        }
+    }
+}
