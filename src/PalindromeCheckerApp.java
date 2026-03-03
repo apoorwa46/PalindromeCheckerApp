5a78ade1c14927d6fe683d4e154b951ed60bbc18
apoorwa46 <apoorwa466@gmail.com>
2026-03-03 14:19:57 +0530

    UC1

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+public class PalindromeCheckerApp {
+
+    static final String APP_NAME = "Palindrome Checker App";
+    static final String VERSION = "Version 1.0";
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("       " + APP_NAME);
+        System.out.println("       " + VERSION);
+        System.out.println("======================================");
+
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("Program started successfully.");
+    }
+}
