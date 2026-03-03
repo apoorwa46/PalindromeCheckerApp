36f211cb939c2ae9539305e6e594e154edf8ee41
apoorwa46 <apoorwa466@gmail.com>
2026-03-03 15:12:22 +0530

    UC10

---
+++ src/UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class UseCase10PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        input = input.toLowerCase();
+
+        input = input.replaceAll("\\s+", "");
+
+        boolean isPalindrome = true;
+
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Result: It is a palindrome (ignoring spaces and case).");
+        } else {
+            System.out.println("Result: It is NOT a palindrome.");
+        }
+
+        sc.close();
+    }
+}
